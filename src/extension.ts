f83a14bc1846a32e0b88205a83793978d79f1214
TimJN17 <tim.j.n17@gmail.com>
2025-01-28 17:52:24 -0500

    (wip) removed active text from this branch

--- src/extension.ts
+++ src/extension.ts
@@ -10,41 +10,6 @@ as raw tesxt on a new line.
 
 Test extension with fn+F5. NOTE: You must use cli: 'npm run compile' before testing any new changes.
 */
-function insertEmojiComment(
-		editor: { 
-			document: any; 
-			selections: any; edit: (arg0: (editBuilder: any) => void) => void; }, 
-		emoji: any, 
-		keyword: any
-	) {
-
-	const document = editor.document;
-	const selections = editor.selections;
-
-	editor.edit(editBuilder => {
-		selections.forEach((selection: { start: { line: any; }; }) => {
-			const line = document.lineAt(selection.start.line);
-			const lineText = line.text;
-
-			// Check if the line is already a comment
-			const commentPrefixes = ["//", "#", "/*", '"""', "--"];
-			const isComment = commentPrefixes.some(prefix => lineText.trim().startsWith(prefix));
-
-			if (isComment) {
-
-				// add the emoji in front of the keyword
-				if (!lineText.includes(`${emoji} ${keyword}`)) {
-					const updatedLine = lineText.replace(keyword, `${emoji} ${keyword}`)
-					editBuilder.replace(line.range, updatedLine);
-				}
-			} else {
-				// add the emoji if is new line
-				const updatedLine = `${emoji} ${keyword}`
-				editBuilder.insert(line.range.start, updatedLine + "\n")
-			}
-		})
-	})
-}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -53,12 +18,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "naudetMilComs" is now active!');
-
-	let disposable = vscode.commands.registerCommand("HelloWorld.helloWorld", () => {
-
-		vscode.window.showInformationMessage("Hello World!");
-
-	});
 
 	// register the milComms completion provider for python; first parameter is { scheme: "file", lanuage: "plaintext"}, or just "python"
 	let provider = vscode.languages.registerCompletionItemProvider("python", {
@@ -73,10 +32,7 @@ export function activate(context: vscode.ExtensionContext) {
 			const line = document.lineAt(position).text;
 			console.log(linePrefix);
 
-			if (linePrefix.includes("DANGER") 
-				|| linePrefix.includes("# DANGER") 
-				|| linePrefix.includes('"""DANGER"""') 
-				|| line.includes("DANGER")) {
+			if (linePrefix.includes("DANGER") || line.includes("DANGER")) {
 				const dangerCompletion = new vscode.CompletionItem("DANGER comment");
 				const dangerDocString = new vscode.CompletionItem("DANGER Doc String");
 
@@ -142,32 +98,7 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	}, 'DANG', 'ALER', 'CAUT', 'SUCC'); // Trigger completion after typing the few letters of each phrase
 
-	let editor = vscode.commands.registerCommand("extension.insertEmojies", async () => {
-		vscode.window.showInformationMessage("Active Text Editor is in play!")
-		const activteText = vscode.window.activeTextEditor;
-		if (activteText) {
-
-			// define inital variables from the activetext editor
-			const document = activteText.document;
-			const selection = activteText.selection;
-
-			// get range from the start of the line to the cursor's selection
-			const range = selection.isEmpty ? document.lineAt(selection.start.line).range : selection;
-			const text = document.getText(range);
-
-			// replacement lines
-			if (text.includes("DANGER")) {
-				activteText.edit(editBuilder => {
-					const newText = text.replace("DANGER", "❗DANGER");
-					editBuilder.replace(range, newText)
-				});
-			} else {
-				vscode.window.showInformationMessage("[ INFO ]: No ' DANGER' found to replace.");
-			}
-		}
-	});
-
-	context.subscriptions.push(disposable, provider, editor);
+	context.subscriptions.push(provider);
 
 // END 'activate' vscodeExtensionContext function
 }
